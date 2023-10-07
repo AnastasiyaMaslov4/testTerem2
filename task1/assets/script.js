193348ed2c22b420cae8a37466010275daec549c
Anastasiya <108454069+AnastasiyaMaslov4@users.noreply.github.com>
2023-10-07 13:12:22 +0300

    Add files via upload

--- task1/assets/script.js
+++ task1/assets/script.js
@@ -3,18 +3,19 @@ const closeBtn = document.querySelector('.modal-window__close');
 const overlay = document.querySelector('.overlay');
 const titleBlock = document.querySelector('.first-section');
 const secondBlock = document.querySelector('.block-swipe');
-
+const btn1 = document.querySelector('.btn-warning');
+const btn2 = document.querySelector('.btn-success');
 
 function closeModal() {
     modalWindow.style.display = 'none';
     overlay.style.display = 'none';
 };
 
-function hideTitle() {
-    titleBlock.classList.toggle('hidden');
-}
+btn1.addEventListener('click', () => titleBlock.classList.toggle('hidden'));
 
-function swipeBlocks() {
-    secondBlock.classList.toggle('first-block');
-}
+btn2.addEventListener('click', () => secondBlock.classList.toggle('first-block'))
+
+overlay.addEventListener('click', closeModal);
+
+closeBtn.addEventListener('click', closeModal);
 
